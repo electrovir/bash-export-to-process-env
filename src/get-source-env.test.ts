d739a36987c7fdff3f9e888b0ba1eb544cb223e5
electrovir <electrovir@users.noreply.github.com>
2024-01-10 21:25:00 +0000

    [dev] fix tests and package lock

--- src/get-source-env.test.ts
+++ src/get-source-env.test.ts
@@ -1,19 +1,57 @@
 import {itCases} from '@augment-vir/chai';
-import {describe} from 'mocha';
+import {awaitedForEach, filterObject, typedHasProperty} from '@augment-vir/common';
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
 import {testFilePaths} from './dev-file-paths';
 import {getNewEnvFromScript} from './get-source-env';
 
 describe(getNewEnvFromScript.name, () => {
-    itCases(getNewEnvFromScript, [
+    const testCases: ReadonlyArray<{
+        it: string;
+        filePath: string;
+        narrowedExpectedOutput: Readonly<Record<string, string>>;
+    }> = [
         {
             it: 'should work on simple bash file with some edge cases',
-            input: testFilePaths.exampleBash,
-            expect: {
+            filePath: testFilePaths.exampleBash,
+            narrowedExpectedOutput: {
                 EXAMPLE_VAR: 'hello-there',
                 WITH_SPACE: 'herp',
-                WITH_INTERPOLATION: 'herp',
                 WITH_SPACE_IN_QUOTES: 'herp derp',
             },
         },
-    ]);
+    ];
+
+    async function testGetNewEnvFromScript(
+        filePath: string,
+        expected: Readonly<Record<string, string>>,
+    ) {
+        const output = filterObject(await getNewEnvFromScript(filePath), (key) => {
+            return typedHasProperty(expected, key);
+        });
+
+        return output;
+    }
+
+    itCases(
+        testGetNewEnvFromScript,
+        testCases.map((testCase) => {
+            return {
+                it: testCase.it,
+                expect: testCase.narrowedExpectedOutput,
+                inputs: [
+                    testCase.filePath,
+                    testCase.narrowedExpectedOutput,
+                ] as const,
+            };
+        }),
+    );
+    it('should create expected outputs', async () => {
+        await awaitedForEach(testCases, async (testCase) => {
+            const output = filterObject(await getNewEnvFromScript(testCase.filePath), (key) => {
+                return typedHasProperty(testCase.narrowedExpectedOutput, key);
+            });
+            assert.deepStrictEqual(output, testCase.narrowedExpectedOutput);
+        });
+    });
 });
